571232aeb2763fb4eebbaff49c9424d73ca302eb
Cory Chambers <corymchambers1@gmail.com>
2022-05-02 19:01:58 -0400

    Initalized and configured AWS Amplify. Added Amplify Auth and configured social logins.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "warnty1db438e4": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
